bc6288fe37000d9eca71ae66e8e7caac1cf36da9
P-Saha <priyonto2000@gmail.com>
2017-06-15 17:42:01 -0400

    One Day Mooooreee
    Combined

--- Kittens/core/src/com/mygdx/game/KittensGame.java
+++ Kittens/core/src/com/mygdx/game/KittensGame.java
@@ -31,14 +31,19 @@ public class KittensGame extends ApplicationAdapter{
 	int jumpAt=15;
 	private Kitten kat;
 	private SideEnemy enemy;
+	private Pages page;
 	Level curLevel;
 	Level [] levelist;
 	Level town;
-	Pixmap forestMap;
-	int forestWall;
-	Texture forestBG,bulletPic;
+	Pixmap forestMap,mainMap;
+	int forestWall,mapWall;
+	Texture forestBG,mapBG,mainMenu;
+	Texture bulletPic;
 	boolean jumped=false;
 	boolean wUp=false;
+	//boolean playing = false;
+	boolean playing = true;
+	
 	//170 y is ground for now
 	public void modeShift(){
 		if(Gdx.input.isKeyJustPressed(Keys.X)){
@@ -65,7 +70,7 @@ public class KittensGame extends ApplicationAdapter{
 	}
 	public void drawBack(){
 		if (mode.equals("map")){
-			batch.draw(forestBG,0,0);
+			batch.draw(mapBG,0,0);
 		}
 		if (mode.equals("side")){
 			batch.draw(forestBG,0,0);
@@ -75,7 +80,7 @@ public class KittensGame extends ApplicationAdapter{
 		return (r<<24) + (g<<16) + (b<<8) + a;
 	}
 	class Kitten{
-		private int mapx,mapy,sidex,sidey,vy,health,mapCurAction,sideCurAction,oldCurAction,jumpDirect;
+		private int mapx,mapy,sidex,sidey,vy,health,mapCurAction,sideCurAction,oldCurAction,jumpDirect,limitDamage;
 		private boolean alive,attacking,drawn;
 		private Texture [][] mapFrames= new Texture[4][7];
 		//Left,Right,Up,Down
@@ -95,7 +100,7 @@ public class KittensGame extends ApplicationAdapter{
 			sidex=xx;
 			sidey=yy;
 			vy=0;
-			health=10;
+			health=100000;
 			mapFrames=mapframelist;
 			sideFrames=sideframelist;
 			sideIdleFrames=sideidleframelist;
@@ -112,6 +117,26 @@ public class KittensGame extends ApplicationAdapter{
 		}
 		public void collide(SideEnemy enemy){
 			if (hitbox.overlaps(enemy.getHitbox())){
+				//System.out.println("hit");
+				limitDamage+=1;
+				if (limitDamage==1){
+					takeHealth(5);
+				}
+			}
+			if (enemy.checkBoundary(150)==false){
+				limitDamage=0;
+			}
+			//System.out.println(health);				
+			//System.out.println(limitDamage);
+		}
+		public void takeHealth(int damage){
+			if ((health-damage)>=damage){
+				health-=damage;
+				alive=true;
+			}
+			else{
+				health=0;
+				alive=false;
 			}
 		}
 		public void idleDraw(){
@@ -158,7 +183,7 @@ public class KittensGame extends ApplicationAdapter{
 						Bullet katBullet = new Bullet(sidex,sidey,bulletPic,jumpDirect);
 						bullets.add(katBullet);
 					}
-					if (curAtkFrame>5){///////////////////////////////////////////////////////
+					if (curAtkFrame>20){///////////////////////////////////////////////////////
 						attacking=false;
 						curAtkFrame=0;
 					}
@@ -169,14 +194,16 @@ public class KittensGame extends ApplicationAdapter{
 			drawn=false;
 			if (alive && !attacking){
 				if(Gdx.input.isKeyPressed(Keys.A)||Gdx.input.isKeyPressed(Keys.LEFT)){
-					if (mode.equals("map")){
+					if (mode.equals("map") && checkClear(mapx-10,mapy,mainMap,mapWall)==true){
+						//checkClear(kat.getSideX(),kat.getSideY(),forestMap,forestWall);
+						//checkClear(kat.getSideX(),kat.getSideY(),mainMap,mapWall);
 						if(mapx>0){
 							mapx-=10;
 						}
 						mapCurAction=LEFT;
 						drawn=true;
 					}
-					if (mode.equals("side")){
+					if (mode.equals("side") && checkClear(sidex-10,sidey,forestMap,forestWall)==true){
 						if ((sidex-10)>=0){
 							sidex-=10;
 						}
@@ -187,14 +214,14 @@ public class KittensGame extends ApplicationAdapter{
 					}
 				}
 				if(Gdx.input.isKeyPressed(Keys.D)||Gdx.input.isKeyPressed(Keys.RIGHT)){
-					if (mode.equals("map")){
+					if (mode.equals("map") && checkClear(mapx+10,mapy,mainMap,mapWall)==true){
 						if(mapx<2048-500){
 							mapx+=10;
 						}
 						mapCurAction=RIGHT;
 						drawn=true;
 					}
-					if (mode.equals("side")){
+					if (mode.equals("side") && checkClear(sidex+10,sidey,forestMap,forestWall)==true){
 						if ((sidex+130)<=1456){
 							sidex+=10;
 						}
@@ -205,14 +232,14 @@ public class KittensGame extends ApplicationAdapter{
 					}
 				}
 				if(Gdx.input.isKeyPressed(Keys.W)||Gdx.input.isKeyPressed(Keys.UP)){
-					if (mode.equals("map")){
+					if (mode.equals("map") && checkClear(mapx,mapy+10,mainMap,mapWall)==true){
 						if(mapy<1024-200){
 							mapy+=10;
 						}
-						//mapCurAction=UP;
+						mapCurAction=UP;
 						drawn=true;
 					}
-					if (mode.equals("side")){
+					if (mode.equals("side") && checkClear(sidex,sidey+10,forestMap,forestWall)==true){
 						if(sidey<=190){
 							vy=20;
 							jumpAt=sidey;
@@ -221,15 +248,22 @@ public class KittensGame extends ApplicationAdapter{
 					
 				}
 				if(Gdx.input.isKeyPressed(Keys.S)||Gdx.input.isKeyPressed(Keys.DOWN)){
-					if (mode.equals("map")){
+					if (mode.equals("map") && checkClear(mapx,mapy-10,mainMap,mapWall)==true){
 						if(mapy<1000){
 							mapy-=10;
 						}
-						//mapCurAction=DOWN;
+						mapCurAction=DOWN;
+						drawn=true;
+					}
+					else if (mode.equals("side") && checkClear(sidex,sidey-10,forestMap,forestWall)==true){
+						if(sidey<1000){
+							sidey-=10;
+						}
+						sideCurAction=DOWN;
 						drawn=true;
 					}
 				}
-				if (mode.equals("side")){
+				if (mode.equals("side") && checkClear(sidex,sidey,forestMap,forestWall)==true){
 					vy-=1;
 					if(170<sidey+vy){
 						sidey+=vy;
@@ -257,13 +291,14 @@ public class KittensGame extends ApplicationAdapter{
 		public int getMapY(){return mapy;}
 		public int getSideX(){return sidex;}
 		public int getSideY(){return sidey;}
+		public int getSideCurAction(){return sideCurAction;}
 		public Rectangle getHitbox(){return hitbox;}
 	}
 	class SideEnemy{
 		private int x,y,health,curFrame,curAction,speed;
 		private double count;
 		private Texture[][]frames;
-		private boolean alive;
+		private boolean alive,attacking,topClear=true,inBound=false;
 		private Rectangle hitbox;
 		private SideEnemy(int xx,int yy,Texture[][]framelist, int spd){
 			x=xx;
@@ -295,6 +330,60 @@ public class KittensGame extends ApplicationAdapter{
 				curFrame=(int)(count)%7;
 			}
 		}
+		public boolean checkAbove(){
+			if (kat.getSideCurAction()==UP){
+				if (kat.getSideX()>=(x-150) && kat.getSideX()<(x+150) && kat.getSideY()<=y+150){
+					topClear=false;
+				}
+				else{
+					topClear = true;
+				}
+			}
+			return topClear;
+		}
+		public boolean checkBoundary(int distance){
+			int enemyKatDistance = Math.abs(x-kat.getSideX());
+			//System.out.println(enemyKatDistance);
+			if (enemyKatDistance<=distance){
+				inBound = true;
+			}
+			else{
+				inBound=false;
+			}
+			return inBound;
+		}
+		public void attack(){
+			if (mode.equals("side")){
+				checkAbove();
+				if ((int)count%5==0){
+					attacking = true;
+					if (checkAbove()==true){
+						Bullet sideEnBullet = new Bullet(x,y,bulletPic,curAction);
+						bullets.add(sideEnBullet);
+					}
+					else{
+						Bullet sideEnBullet = new Bullet(x,y,bulletPic,UP);
+						bullets.add(sideEnBullet);
+					}
+				}
+				else{
+					attacking=false;
+				}
+			}
+		}
+		public void smartAttack(){
+			if (mode.equals("side")){
+				checkAbove();
+				if (checkBoundary(250)==true){
+					attacking = true;
+					attack();
+				}
+				else{
+					attacking=false;
+				}
+			//System.out.println(inBound);
+			}
+		}
 		public void draw(){
 			if (mode.equals("side")){
 				batch.draw(frames[curAction][curFrame],x,y);
@@ -306,36 +395,95 @@ public class KittensGame extends ApplicationAdapter{
 		public boolean isAlive(){return alive;}
 		public Rectangle getHitbox(){return hitbox;}
 	}
-	public boolean checkClear(int x,int y){
+	public boolean checkClear(int x,int y,Pixmap map, int wall){
+		if(x<0 || x>= map.getWidth() || y<0 || y>= map.getHeight()){
+			//System.out.println(false);
+			return false;
+		}
+		int c = map.getPixel(x, map.getHeight()- y);
+		//System.out.println(c!=wall);
+		return c != wall;
+		/*
 		if(x<0 || x>= forestMap.getWidth() || y<0 || y>= forestMap.getHeight()){
 			return false;
 		}
 		int c = forestMap.getPixel(x, forestMap.getHeight()- y);
-		return c != forestWall;
+		return c != forestWall;*/
 	}
 	public void moveBullet(){
+		int limitDamage=0;
 		batch.begin();
-		if (mode.equals("side")){
-			if (bullets.size()>0){
-				for (int i=0;i<bullets.size();i++){
-					Bullet bull = bullets.get(i);
-					batch.draw(bulletPic,bull.bx,bull.by);
-					if (bull.getBullDirect()==LEFT){
-						bull.bulletLeft();
-						if (bull.bx<=0){
-							bullets.remove(bull);
-						}
+		if (bullets.size()>0){
+			for (int i=0;i<bullets.size();i++){
+				Bullet bull = bullets.get(i);
+				batch.draw(bulletPic,bull.bx,bull.by);
+				double startbx=bull.bx;
+				double startby=bull.by;
+				bull.getHitbox().set(bull.bx,bull.by,bulletPic.getWidth(),bulletPic.getHeight());
+				limitDamage+=1;
+				if (startbx==kat.getSideX() && startby==kat.getSideY()){
+					if (bull.getHitbox().overlaps(kat.getHitbox()) && limitDamage==1){
+						kat.takeHealth(10);
 					}
-					else if (bull.getBullDirect()==RIGHT){
-						bull.bulletRight();
-						if (bull.bx>=1456){
-							bullets.remove(bull);
-						}
+					else{
+						limitDamage=0;
+					}
+				}
+				if (bull.getBullDirect()==LEFT){
+					bull.bulletLeft();
+					if (bull.bx<=0){
+						bullets.remove(bull);
+					}
+				}
+				else if (bull.getBullDirect()==RIGHT){
+					bull.bulletRight();
+					if (bull.bx>=1456){
+						bullets.remove(bull);
+					}
+				}
+				else if(bull.getBullDirect()==UP){
+					bull.bulletUp();
+					if (bull.by>=900){
+						bullets.remove(bull);
 					}
 				}
 			}
 		}
 		batch.end();
+	}
+	class Bullet{
+		int bx,by,startBx,startBy,direct;
+		Texture bulletPic;
+		private Rectangle hitbox;
+		public Bullet (int kx, int ky,Texture bulletPic,int direction){
+			if (direction == LEFT){
+				bx = kx+10;
+			}
+			else{
+				bx = kx+100;
+			}
+			by = ky+65;
+			this.bulletPic = bulletPic;
+			direct = direction;
+			startBx = kx;
+			startBy = ky;
+			hitbox=new Rectangle(0,0,0,0);
+		}
+		public void bulletLeft(){
+			bx-=5;
+		}
+		public void bulletRight(){
+			bx+=5;
+		}
+		public void bulletUp(){
+			by+=5;
+		}
+		public int getBullDirect(){
+			return direct;
+		}
+		public int getStartBx(){return startBx;}
+		public int getStartBY(){return startBy;}
+		public Rectangle getHitbox(){return hitbox;}
 	}
 	class Level{
 		private SideEnemy [] enemies;
@@ -374,51 +522,42 @@ public class KittensGame extends ApplicationAdapter{
 		public int getMaxX(){return maxX;}
 		
 	}
-	class Bullet{
-		int bx,by,direct;
-		Texture bulletPic;
-		public Bullet (int kx, int ky,Texture bulletPic,int direction){
-			bx = kx;
-			by = ky;
-			this.bulletPic = bulletPic;
-			direct = direction;
+	class Pages{
+		private Texture [][] pages= new Texture[3][2];
+		private int curPage,curFrame,count;
+		public Pages(int page,Texture[][]pagelist){
+			curPage = page;
+			curFrame=0;
+			count=0;
+			pages = pagelist;
 		}
-		public void bulletLeft(){
-			bx-=5;
+		public void changePage(int page){
+			curPage = page;
 		}
-		public void bulletRight(){
-			bx+=5;
-		}
-		public int getBullDirect(){
-			return direct;
+		public void drawPages(){
+			batch.draw(pages[curPage][curFrame],0,0);
+			count += .0625;
+			curFrame=(int)(count);
+			//System.out.println(count);
+			
+			//System.out.println(curFrame);
 		}
 	}
 	@Override
-	public void create () {
+	public void create(){
 		DisplayMode dm=Gdx.graphics.getDesktopDisplayMode();
 		Gdx.graphics.setDisplayMode(dm.width,dm.height,false);
-		font = new BitmapFont(Gdx.files.internal("font.fnt"));
 		batch = new SpriteBatch();
 		camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		forestWall = cNum(0,0,255,255);
+		forestWall = cNum(255,0,0,255);
+		mapWall = cNum(255,0,0,255);
+		forestMap = new Pixmap(Gdx.files.internal("ForestMask.png"));
+		mainMap = new Pixmap(Gdx.files.internal("MapMask.png"));
 		levelist=new Level[4];
 		Texture[][]mapFrames=new Texture[4][7];
 		Texture[][]sideFrames=new Texture[7][7];
 		Texture[][]sideIdleFrames=new Texture[2][7];
-		/*for(int i=0;i<4;i++){
-			for(int j=0;j<7;j++){
-				mapFrames[i][j]=new Texture("mapFrames"+(i+2)+"-"+(j+1)+".png");
-			}
-		}
-		for(int i=0;i<5;i++){
-			for(int j=0;j<7;j++){
-				sideFrames[i][j]=new Texture("sideFrames"+(i+2)+"-"+(j+1)+".png");
-			}
-		}
-		for(int i=0;i<14;i++){
-			sideIdleFrames[i/7][i%7+1]=new Texture("sideFrames1-"+(i+1)+".png");
-		} 
-		*/ 
+		Texture[][]pages = new Texture[3][2];
 		for(int i=0;i<7;i++){
 			sideIdleFrames[0][i]=new Texture("sideFrames1-"+(i+1)+".png");
 			sideIdleFrames[1][i]=new Texture("sideFrames1-"+(i+8)+".png");
@@ -434,25 +573,19 @@ public class KittensGame extends ApplicationAdapter{
 				mapFrames[i][j]=new Texture("sideFrames"+(i%2+2)+"-"+(j+1)+".png");
 			}
 		}
-		/*for(int i=0;i<2;i++){
-			for(int j=0;j<7;j++){
-				sideIdleFrames[i][j]=new Texture("sideFrames"+(i+1)+"-"+(j+1)+".png");
-			}
-		} 
-		for(int i=0;i<4;i++){
-			for(int j=0;j<7;j++){
-				mapFrames[i][j]=new Texture("sideFrames"+(i%2+1)+"-"+(j+1)+".png");
-			}
+		for (int i=0;i<2;i++){
+			pages[0][i] = new Texture("menu1-"+(i+1)+".png");
 		}
-		for(int i=0;i<2;i++){
-			for(int j=0;j<7;j++){
-				sideFrames[i][j]=new Texture("sideFrames"+(i+1)+"-"+(j+1)+".png");
-			}
-		}*/
 		forestBG = new Texture("Forest1.png");
+		mapBG = new Texture("Mao.jpg");
+		//forestBG = new Texture("ForestMask.png");
+		//mapBG = new Texture("MapMask.png");
+		mainMenu= new Texture("menu.jpg");
 		bulletPic=new Texture("Bullet.png");
 		kat=new Kitten(10,170,mapFrames,sideFrames,sideIdleFrames);
-		enemy=new SideEnemy(10,170,sideIdleFrames,3);
+		enemy=new SideEnemy(1500,170,sideIdleFrames,3);
+		page = new Pages(0,pages);
+		//main menu = 0
 		SideEnemy[] testEnemyList=new SideEnemy[1];
 		testEnemyList[0]=enemy;
 		Level Forest= new Level (testEnemyList,forestBG, mapFrames[0][0],500, 500);
@@ -467,7 +600,7 @@ public class KittensGame extends ApplicationAdapter{
 		float camY=Math.max(Gdx.graphics.getHeight()/2+32, Math.min(forestBG.getHeight()-Gdx.graphics.getHeight()/2-20, kat.getSideY()));
 		if (mode.equals("map")){
 			camX=Math.max(Gdx.graphics.getWidth()/2+6, Math.min(forestBG.getWidth()-Gdx.graphics.getWidth()/2-6, kat.getMapX()));
-			camY=Math.max(Gdx.graphics.getHeight()/2+32, Math.min(forestBG.getHeight()-Gdx.graphics.getHeight()/2-32, kat.getMapY()));
+			camY=Math.max(Gdx.graphics.getHeight()/2-20, Math.min(forestBG.getHeight()-Gdx.graphics.getHeight()/2-20, kat.getMapY()));
 		}
 		//jan helped me here
 		camera.position.set(camX,camY,0);
@@ -479,25 +612,35 @@ public class KittensGame extends ApplicationAdapter{
 		//Always stuff and then drawing
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		modeShift();
-		updateCamera();
-		kat.move();
-		for (SideEnemy enemy:curLevel.getEnemies()){
-			enemy.move();
+		if (playing==true){
+			modeShift();
+			updateCamera();
+			kat.move();
+			for (SideEnemy enemy:curLevel.getEnemies()){
+				enemy.move();
+			}
+			//enemy.move();
+			batch.begin();
+			drawBack();
+			kat.draw();
+			kat.attackAndDraw();
+			for (SideEnemy enemy:curLevel.getEnemies()){
+				enemy.draw();
+			}
+			//enemy.draw();
+			for (Level level:levelist){
+				level.drawIcon();
+			}
+			batch.end();
+			//enemy.attack();
+			enemy.smartAttack();
+			moveBullet();
+			kat.collide(enemy);
 		}
-		//enemy.move();
-		batch.begin();
-		drawBack();
-		kat.draw();
-		kat.attackAndDraw();
-		for (SideEnemy enemy:curLevel.getEnemies()){
-			enemy.draw();
+		else{
+			batch.begin();
+			page.drawPages();
+			batch.end();
 		}
-		for (Level level:levelist){
-			level.drawIcon();
-		}
-		batch.end();
-		moveBullet();
-		kat.collide(enemy);
 	}
 }
